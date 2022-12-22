852509c78b2ca644b3a9a2c887c3145fd365272c
Felipe Souza <felipe.souza@cwi.com.br>
2022-12-22 18:52:43 -0300

    Collects metrics

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,25 @@
 import { Router } from 'express';
+import { Counter, Histogram } from 'prom-client';
 
 const orderRouter = Router({ mergeParams: true });
+const orderCounter = new Counter({
+    name: 'total_orders',
+    help: 'Total orders made'
+});
+const orderPriceHistogram = new Histogram({
+    name: 'total_order_price',
+    help: 'Total price',
+    buckets: [1, 5, 15, 50, 100, 500, 1000, 5000],
+});
+
 
 orderRouter.post('/finishOrder', async (req, res, next) => {
     try {
         const { totalPrice } = req.body;
 
         console.log('Just finished an order.', JSON.stringify(req.body));
-        // TODO count total orders
-        // TODO accumulate total value sold
+        orderCounter.inc();
+        orderPriceHistogram.observe(totalPrice);
 
         return res.status(200).json({
             status: 'SOLD',
--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import promBundle from 'express-prom-bundle';
+
+const metricsMiddleware = promBundle({includeMethod: true});
 
 export function server(routes, port = 3000) {
     const app = express();
 
     app.use(bodyParser.json())
+    app.use(metricsMiddleware);
 
     for (const route of routes) {
         app.use(route);
